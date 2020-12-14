08079fccbfd711e051699e1178fd2a3b460d7573
Jason Melnikel <jmelnikel@gmail.com>
2020-12-14 16:40:35 -0500

    comment

--- server/lib/util/simulate-delay.js
+++ server/lib/util/simulate-delay.js
@@ -8,15 +8,17 @@
 // This is used to make the front-end behaviour a little more
 // realistic even while we use a simplistic "in-memory" db.
 
+// The delay has been disabled as it was for production purposes only.
 function someMilliseconds() {
   return 0;
 }
-
-// function someMilliseconds() {
-//   return Math.floor(Math.random() * 400) + 100;
-// }
 
 module.exports = function simulateDelay(callback) {
   setTimeout(callback, someMilliseconds());
 }
 
+
+
+// function someMilliseconds() {
+//   return Math.floor(Math.random() * 400) + 100;
+// }
